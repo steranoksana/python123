81481ea93715428450c72c30efcd29f3beb83b65
Oksana Vasyliuk <streranoksana@gmail.com>
2023-09-10 16:34:31 +0300

    added print

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
-# Запитуємо в користувача три числа
+# # Запитуємо в користувача три числа
+#
+# num1 = float(input("Enter the first number: "))
+#
+# num2 = float(input("Enter the second number: "))
+#
+# num3 = float(input("Enter the third number: "))
+#
+# # Обчислюємо середнє арифметичне значення
+#
+# average = (num1 + num2 + num3) / 3
+#
+# # Виводимо результат
+#
+# print("arithmetic mean:", average)
 
-num1 = float(input("Enter the first number: "))
+# meters = int(input("Введите кол-во метров: "))
+#
+# what = int(input("Во что вы хотите перевести метры? Мили - 1\n Дюймы - 2\n Ярды - 3"))
+#
+# if what == 1:
+#
+#    print(f"{meters} метров = {meters*0.000621} миль")
+#
+# elif what == 2:
+#
+#    print(f"{meters} метров = {meters*39.37} дюймов")
+#
+# elif what == 3:
+#
+# print("arithmetic mean:", average)
 
-num2 = float(input("Enter the second number: "))
-
-num3 = float(input("Enter the third number: "))
-
-# Обчислюємо середнє арифметичне значення
-
-average = (num1 + num2 + num3) / 3
-
-# Виводимо результат
-
-print("arithmetic mean:", average)
+print ("Hello world")
